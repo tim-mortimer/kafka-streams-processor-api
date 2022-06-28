9b39205c05ea219eec44bedbd7c40b03f8723e2e
Tim Mortimer <tim@kiteframe.co.uk>
2022-06-28 11:35:35 +0100

    Bootstrap project

---
+++ src/main/java/com/example/processorapi/KafkaStreamsConfig.java
@@ -0,0 +1,33 @@
+package com.example.processorapi;
+
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
+import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
+import org.springframework.kafka.config.KafkaStreamsConfiguration;
+
+import java.util.Map;
+
+@Configuration
+@EnableKafkaStreams
+public class KafkaStreamsConfig {
+
+    @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
+    public KafkaStreamsConfiguration kStreamsConfig() {
+        return new KafkaStreamsConfiguration(Map.of(
+                StreamsConfig.APPLICATION_ID_CONFIG, "testStreams",
+                StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+        ));
+    }
+
+    @Bean
+    public KStream<String, String> kStream(StreamsBuilder streamsBuilder) {
+        KStream<String, String> stream = streamsBuilder.stream("inputTopic");
+        stream.mapValues(v -> v + "-a")
+                .to("outputTopic");
+        return stream;
+    }
+}
---
+++ src/test/java/com/example/processorapi/ProcessorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.processorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
